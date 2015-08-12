a5005dcd4ae8e4fb1368e96da3b71cdc3dca110e
Zack Boehm <git@zackboehm.com>
2015-08-12 00:16:49 -0400

    init

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/*global -$ */
+'use strict';
+// generated on 2015-08-11 using generator-gulp-webapp 0.3.0
+var gulp = require('gulp');
+var jsonminify = require('gulp-jsonminify');
+var jsonlint = require('gulp-json-lint');
+
+var jsonLintReporter = function (lint, file) {
+    console.log(file.path + ': ' + lint.error);
+    if(lint.error){
+      process.exit(1);
+    }
+};
+
+gulp.task('minify', function () {
+    return gulp.src(['json/*.json'])
+        .pipe(jsonminify())
+        .pipe(jsonlint())
+        .pipe(jsonlint.report(jsonLintReporter))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['minify'], function () {
+  return gulp.src('dist/**/*');
+});
+
+gulp.task('default', function () {
+  gulp.start('build');
+});
